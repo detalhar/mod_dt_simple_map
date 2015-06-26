5a27e5b855ce1ad8693af9f32e702ffdf92c3986
Welkson Ramos <welksonr@hotmail.com>
2015-06-26 18:19:16 -0300

    update

--- mod_dt_simple_map.php
+++ mod_dt_simple_map.php
@@ -1,13 +1,20 @@
 <?php
 
-/*---------------------------------------------------------------------------------
-# mod_dt_simple_map - Google Map module for Joomla by Detalhar - DetalharWeb.com.br
-# ---------------------------------------------------------------------------------
-# author    Detalhar http://www.detalharweb.com.br
-# Copyright (C) 2015 DetalharWeb.com.br. All Rights Reserved.
-# @license - GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
-# Website: http://www.detalharweb.com.br
-----------------------------------------------------------------------------------*/
+/**
+*-------------------------------------------------------------------------------
+* DT Simple Map - Google Map module for Joomla by Detalhar - DetalharWeb.com.br
+*-------------------------------------------------------------------------------
+* @package DT Simple Map
+* @version 1.0.0
+* @author Detalhar http://www.detalharweb.com.br
+* @copyright (C) 2015 Detalhar. All Rights Reserved
+* @license - GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html or see LICENSE.txt
+* @website: http://www.detalharweb.com.br
+* mod_dt_simple_map is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+**/
 
 defined('_JEXEC') or die ('Restricted Access');
 $doc = JFactory::getDocument();
@@ -16,12 +23,13 @@ $doc->addScript('https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false');
 //Params
 $lat 				= $params->get('lat');
 $lng 				= $params->get('lng');
-$height 			= $params->get('height', 300);
+$height 			= (int)$params->get('height', 300);
 $zoomLevel 			= $params->get('zoomlevel',8);
 $mapTypeId 			= $params->get('maptypeId', 'ROADMAP');
 $marker 			= $params->get('marker');
 $ContentInfo 		= $params->get('infoWindow');
 $panControl 		= $params->get('panControl', false);
 
+$image = trim($marker);
 $moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
 require JModuleHelper::getLayoutPath('mod_dt_simple_map', $params->get('layout','default'));
--- tmpl/default.php
+++ tmpl/default.php
@@ -1,53 +1,84 @@
-<?php 
-/*---------------------------------------------------------------------------------
-# mod_dt_simple_map - Google Map module for Joomla by Detalhar - DetalharWeb.com.br
-# ---------------------------------------------------------------------------------
-# author    Detalhar http://www.detalharweb.com.br
-# Copyright (C) 2015 DetalharWeb.com.br. All Rights Reserved.
-# @license - GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
-# Website: http://www.detalharweb.com.br
-----------------------------------------------------------------------------------*/
+<?php
+
+/**
+*-------------------------------------------------------------------------------
+* DT Simple Map - Google Map module for Joomla by Detalhar - DetalharWeb.com.br
+*-------------------------------------------------------------------------------
+* @package DT Simple Map
+* @version 1.0.0
+* @author Detalhar http://www.detalharweb.com.br
+* @copyright (C) 2015 Detalhar. All Rights Reserved
+* @license - GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html or see LICENSE.txt
+* @website: http://www.detalharweb.com.br
+* mod_dt_simple_map is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+**/
 
 defined('_JEXEC') or die('Restricted Access');
 
-
- ?>
-
-  <div class="dt-simple-map<?php echo $moduleclass_sfx; ?>">
- 	<div id="dt-simple-map-canvas" style="margin:0; padding:0;width:100%; height:<?php echo $height?>px;"></div>
- </div>
+?>
+ <?php if($lat && $lng) { ?>
  <script type="text/javascript">
  function initialize(){
- 	var myLatLng = new google.maps.LatLng(<?php echo $lat?>, <?php echo $lng ?>);
- 	var mapOptions = {
- 		center: myLatLng,
+ 	var map;
+ 	var myLatlng;
+ 	var mapOptions;
+ 	var contentString;
+ 	var infoWindow;
+ 	var image;
+ 	var marker;
+
+ 	myLatlng = new google.maps.LatLng(<?php echo $lat?>, <?php echo $lng ?>);
+ 	mapOptions = {
+ 		center: myLatlng,
  		panControl: <?php echo $panControl ?>,
  		zoom:<?php echo $zoomLevel ?>,
  		mapTypeId: google.maps.MapTypeId.<?php echo $mapTypeId ?>
  	};	
 
- 	var map = new google.maps.Map(document.getElementById('dt-simple-map-canvas'),
+ 	map = new google.maps.Map(document.getElementById('dt-simple-map-canvas'),
 	mapOptions);
 
-	var contentString = "<?php echo $ContentInfo ?>";
-	
-	var image = "<?php echo $baseUrl .$marker ?>";
-	
-	var infoWindow = new google.maps.InfoWindow({
-		content:contentString,
+	contentString = "<?php echo $ContentInfo ?>";
+
+	infoWindow = new google.maps.InfoWindow({
+		content:contentString
 	});
 
-	var marker = new google.maps.Marker({
-		position:myLatLng,
-		map:map,
-		title: 'Mapa Detalhar',
-	});
+	image = '';
+	
+	<?php if($image!=''){ ?>
+		image = "<?php echo $baseUrl .$marker ?>";
+	<?php  } ?>
+
+	if (image!='' & typeof(image)!='undefined') {
+		marker = new google.maps.Marker({
+			position: myLatlng,
+			map: map,
+			icon: image
+		});
+	}
+	else {
+		marker = new google.maps.Marker({
+			position: myLatlng,
+			map: map
+		});
+	};
 
 	google.maps.event.addListener(marker, 'click', function(){
-		infoWindow.open(map, marker);
+		infoWindow.open(map,marker);
 	});
 
 	    marker.setMap(map);
  }
  initialize();
  </script>
+ <div class="dt-simple-map<?php echo $moduleclass_sfx; ?>">
+ 	<div id="dt-simple-map-canvas" style="margin:0; padding:0;width:100%; height:<?php echo $height?>px;"></div>
+ </div>
+ <?php } else { ?>
+ <p style="text-align:center; color:red"><?php echo JTEXT::_('MOD_DT_SIMPLE_MAP_ERROR'); ?></p> 
+
+ <?php } ?>
